1961433314db07da72df82040b2bbd37c11714c8
Edson Junior <edsonjos61@gmail.com>
2022-07-07 08:35:14 -0300

    Criando nomeação para as rotas.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'index']);
+Route::get('/', [PrincipalController::class, 'index'])->name('home');
 
 Route::get('/sobre-nos', [SobrenosController::class, 'index']);
 
@@ -24,7 +24,7 @@ Route::get('/contato', [ContatoController::class, 'index']);
 
 Route::get('/login', function () {
     return 'login';
-});
+})->name('login');
 
 Route::prefix('/app')->group(function () {
 
@@ -40,28 +40,3 @@ Route::prefix('/app')->group(function () {
         return 'produtos';
     });
 });
-
-/*
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function (
-    string $nome, string $categoria, string $assunto, string $mensagem) {
-        echo "Chegamos aqui..... - $nome - $categoria - $assunto - $mensagem";
-});
-
-// Tornando parâmetro opcionais-> adicionando o sinal de '?' após o mesmo
-// e adicionar valor padrão dentro da função callback.
-//Se todos os parâmetros forem declarados como opcionais e tiverem valores padrões
-//teremos que passar os valores da direita para a esquerda na URL.
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}', function (
-    string $nome,
-    string $categoria,
-    string $assunto,
-    string $mensagem = ''
-) {
-    echo "Chegamos aqui..... - $nome - $categoria - $assunto - $mensagem";
-});
-
-//Aplicando expressões regulares para tratamento dos parâmetros das rotas.
-Route::get('/contato/{nome}/{categoria_id}/', function (string $nome, int $categoria_id = 1) {
-    echo "Chegamos aqui..... - $nome - $categoria_id";
-})->where('nome', '[A-Za-z]+')->where('categoria_id', '[0-9]+');
-*/
