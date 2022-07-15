013f1dfe6c3ae23b2d194ca77f7b9fcb7e23c760
Edson Junior <edson.junior@mereo.com>
2022-07-15 08:07:21 -0300

    Trabalhando com if e else no blade e enviando dados de um array da controller para a view.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,9 +8,10 @@ class TestController extends Controller
 {
     public function test(int $p1, int $p2)
     {
+        $variavelTesteIfElse = ['Fornecedor'];
         //echo "A soma de $p1 e $p2 é: ".($p1 + $p2);
         //return view('site.test', ['p1' => $p1, 'p2' => $p2]);Encaminhando valores recebidos como parâmetro para a view(Array associativo)
         //return view('site.test')->with('p1', $p1)->with('p2', $p2); // método with do laravel
-        return view('site.test', compact('p1', 'p2')); //método compact do laravel
+        return view('site.test', compact('p1', 'p2', 'variavelTesteIfElse')); //método compact do laravel
     }
 }
--- resources/views/site/test.blade.php
+++ resources/views/site/test.blade.php
@@ -3,3 +3,33 @@
     <br>
     P2 = {{ $p2 }}
 </h4>
+
+
+{{-- adicionando comentários com a sintaxe blade --}}
+
+{{-- Incluindo PHP puro nas view do blade--}}
+@php
+    // código em PHP puro.
+    echo 'Teste PHP Puro';
+@endphp
+
+<br>
+{{-- Sintaxe blade --}}
+{{ 'Teste '}}
+<br>
+
+{{-- sintaxe PHP --}}
+<?= 'Teste' ?>
+
+{{-- trabalhando com array no blade --}}
+{{-- @dd($variavelTesteIfElse) --}}
+
+{{-- Blade if-else --}}
+
+@if (count($variavelTesteIfElse) > 0 && count($variavelTesteIfElse) < 10)
+    <h3>Fornecedores cadastrados.</h3>
+@elseif (count($variavelTesteIfElse) > 10 )
+    <h3>Array possui mais de 10 Fornecedores cadastrados.</h3>
+@else
+    <h3>Ainda não existe Fornecedores cadastrados.</h3>
+@endif
