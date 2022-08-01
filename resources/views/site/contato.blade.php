4a6712c114e8933b1fe91037fdbf4d61f2d5aff9
Edson Junior <edson.junior@mereo.com>
2022-08-01 08:18:23 -0300

    Enviando parâmetros(Elementos HTML) para os components do blade via slot(Array associativo). Segunda maneira.

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -10,7 +10,7 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                   @component('site.layouts._components.form_contato')
+                   @component('site.layouts._components.form_contato', ['class' => 'borda-preta'])
                      <p> A nossa equipe analisará a sua mensagem e retornaremos o mais breve possível! </p>
                      <p> Nosso tempo médio de resposta de de 48h </p>
                    @endcomponent
